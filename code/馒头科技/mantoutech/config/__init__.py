50d7995df70d63b1e917a548da751a2dc1ec77bb
dictmap <132747826+dictmap@users.noreply.github.com>
2023-10-09 12:46:49 +0800

    Delete code/馒头科技/mantoutech directory

--- code/馒头科技/mantoutech/config/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ['SENTENCE_TRANSFORMERS_HOME'] = '/raidnvme/czc/SENTENCE_TRANSFORMERS_HOME'
-
-# for huggingface
-if os.path.dirname(__file__).startswith('/home'):
-    os.environ['HF_HOME'] = '/raidnvme/czc/HF_HOME'
-
-# for modelscope
-os.environ['CACHE_HOME'] = '/raidnvme/czc/MODELSCOPE_CACHE_HOME'
